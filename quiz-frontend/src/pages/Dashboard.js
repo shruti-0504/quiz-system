f92e3d02373561944fdde38b96a38b85f803e715
shruti-0504 <guptashruti7710@gmail.com>
2025-04-02 11:20:38 +0530

    dashboard and quiz

--- quiz-frontend/src/pages/Dashboard.js
+++ quiz-frontend/src/pages/Dashboard.js
@@ -1,5 +1,30 @@
-function Dashboard() {
-    return <h1>Dashboard Page</h1>;
-}
+import { useEffect, useState } from "react";
 
-export default Dashboard; 
+const Dashboard = () => {
+    const [quizzes, setQuizzes] = useState([]);
+
+    useEffect(() => {
+        const fetchQuizzes = async () => {
+            const res = await fetch("http://localhost:5000/quiz");
+            const data = await res.json();
+            setQuizzes(data);
+        };
+        fetchQuizzes();
+    }, []);
+
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <h3>Available Quizzes</h3>
+            {quizzes.map((quiz) => (
+                <div key={quiz._id}>
+                    <h4>{quiz.title}</h4>
+                    <p>Start: {new Date(quiz.startTime).toLocaleString()}</p>
+                    <p>End: {new Date(quiz.endTime).toLocaleString()}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Dashboard;
